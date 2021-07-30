baa2725f7d3e1ea5b2e8911fb007b66a45669d0a
Darcy Wang <darcy.wang@lorentz.com.cn>
2021-07-30 16:13:14 +0800

    Test

--- bitcoin.py
+++ bitcoin.py
@@ -5,10 +5,10 @@ from lxml import etree
 from jinja2 import Environment, PackageLoader
 max=10000;min=4000
 #获取环境变量中的MAX，MIN值
-if 'MAX' in os.environ:
-    max=float(os.getenv('MAX')) 
-if 'MIN' in os.environ:
-    min=float(os.getenv('MIN'))
+# if 'MAX' in os.environ:
+#     max=float(os.getenv('MAX')) 
+# if 'MIN' in os.environ:
+#     min=float(os.getenv('MIN'))
 sendemail=False;strategy="继续持有"
 #获取https://price.btcfans.com/上显示的比特币价格
 url='https://price.btcfans.com/'
